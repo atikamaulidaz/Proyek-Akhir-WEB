e23268de7ed3c933e0c8328d547eb4250df5f8d7
atikamaulidaz <2771@smaridasa.web.id>
2022-11-13 20:03:01 +0800

    DB Baru

--- laundry_crud.sql
+++ laundry_crud.sql
@@ -1,17 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
-<<<<<<< HEAD
--- Generation Time: Nov 13, 2022 at 11:13 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
-=======
--- Generation Time: Nov 13, 2022 at 10:46 AM
+-- Generation Time: Nov 13, 2022 at 01:01 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.11
->>>>>>> da0ec80fa9b7f7c361960555cffee24c27043cb4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -35,22 +29,21 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `gambar` (
   `id_gambar` int(11) NOT NULL,
-  `id_laundry` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `nama_file` varchar(64) NOT NULL,
   `file` varchar(64) NOT NULL,
   `waktu` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-<<<<<<< HEAD
-=======
 --
 -- Dumping data for table `gambar`
 --
 
 INSERT INTO `gambar` (`id_gambar`, `id`, `nama_file`, `file`, `waktu`) VALUES
-(31, 50, 'shoes', 'shoes.jfif', '2022-11-13 17:32:19');
+(25, 44, 'siapa', 'siapa.jfif', '2022-11-13 19:16:07'),
+(26, 45, 'kenapa', 'kenapa.jfif', '2022-11-13 19:16:53'),
+(27, 46, 'sepatoooo', 'sepatoooo.jfif', '2022-11-13 19:21:12');
 
->>>>>>> da0ec80fa9b7f7c361960555cffee24c27043cb4
 -- --------------------------------------------------------
 
 --
@@ -59,11 +52,9 @@ INSERT INTO `gambar` (`id_gambar`, `id`, `nama_file`, `file`, `waktu`) VALUES
 
 CREATE TABLE `history` (
   `id_history` int(11) NOT NULL,
-<<<<<<< HEAD
-  `id_laundry` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
-=======
->>>>>>> da0ec80fa9b7f7c361960555cffee24c27043cb4
+  `id_gambar` int(11) NOT NULL,
   `nama` varchar(64) NOT NULL,
   `alamat` varchar(64) NOT NULL,
   `jenis` varchar(64) NOT NULL,
@@ -79,25 +70,24 @@ CREATE TABLE `history` (
 --
 
 CREATE TABLE `laundry` (
-  `id_laundry` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
   `nama` varchar(100) NOT NULL,
   `alamat` text NOT NULL,
-  `telpon` int(100) NOT NULL,
+  `telpon` varchar(25) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `jemis` varchar(100) NOT NULL
+  `jenis` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-<<<<<<< HEAD
-=======
 --
 -- Dumping data for table `laundry`
 --
 
 INSERT INTO `laundry` (`id`, `id_user`, `nama`, `alamat`, `telpon`, `email`, `jenis`) VALUES
-(50, 50, 'Atika M.Z', 'Samarinda', '08521234567', 'atikamaulidazahra@gmail.com', 'Laundry Sepatu');
+(44, 44, 'Atika M.Z', 'Samarinda', '08521234567', 'atikamaulidazahra@gmail.com', 'Regular Ekonomis'),
+(45, 45, 'Atika Maulida Zahra', 'Sempaja', '0987654321', '2771@smaridasa.web.id', 'Laundry Exclusive'),
+(46, 46, 'Rista', 'Jalan Abdul Wahab Syahrani 4 Blok S No. 16', '0987654321', 'chenle@gmail.com', 'Laundry Sepatu');
 
->>>>>>> da0ec80fa9b7f7c361960555cffee24c27043cb4
 -- --------------------------------------------------------
 
 --
@@ -112,6 +102,15 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `nama`, `username`, `password`) VALUES
+(1, 'Atika Maulida Zahra', 'atika', '$2y$10$PZp9ctjmrchSwclm6ku7/eLc.SEK7RjKIJ0rPWkdIJDqHDH9LL9Ee'),
+(3, 'admin', 'admin', '$2y$10$D/loUMYdW36xSxcV1UkfNe0xNcRgMm1JAdH6PR66SXSYT.VsnW/La'),
+(4, 'putri', 'putri', '$2y$10$53ZGcuNrMchtKCAZ49oBCuHjSzDYXYWP7TXio1QBDzKMMwoJ0MvUO');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -120,33 +119,28 @@ CREATE TABLE `user` (
 --
 ALTER TABLE `gambar`
   ADD PRIMARY KEY (`id_gambar`),
-  ADD KEY `id_laundry` (`id_laundry`);
+  ADD KEY `id` (`id`);
 
 --
 -- Indexes for table `history`
 --
 ALTER TABLE `history`
-<<<<<<< HEAD
   ADD PRIMARY KEY (`id_history`),
-  ADD KEY `id_laundry` (`id_laundry`),
-  ADD KEY `id_user` (`id_user`);
-=======
-  ADD PRIMARY KEY (`id_history`);
->>>>>>> da0ec80fa9b7f7c361960555cffee24c27043cb4
+  ADD KEY `fk_history` (`id`),
+  ADD KEY `fk_history1` (`id_user`),
+  ADD KEY `fk_history2` (`id_gambar`);
 
 --
 -- Indexes for table `laundry`
 --
 ALTER TABLE `laundry`
-  ADD PRIMARY KEY (`id_laundry`),
-  ADD KEY `id_user` (`id_user`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id_user`),
-  ADD KEY `id_user` (`id_user`);
+  ADD PRIMARY KEY (`id_user`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -156,11 +150,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `gambar`
 --
 ALTER TABLE `gambar`
-<<<<<<< HEAD
-  MODIFY `id_gambar` int(11) NOT NULL AUTO_INCREMENT;
-=======
-  MODIFY `id_gambar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
->>>>>>> da0ec80fa9b7f7c361960555cffee24c27043cb4
+  MODIFY `id_gambar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `history`
@@ -172,17 +162,13 @@ ALTER TABLE `history`
 -- AUTO_INCREMENT for table `laundry`
 --
 ALTER TABLE `laundry`
-<<<<<<< HEAD
-  MODIFY `id_laundry` int(11) NOT NULL AUTO_INCREMENT;
-=======
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
->>>>>>> da0ec80fa9b7f7c361960555cffee24c27043cb4
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
@@ -192,24 +178,15 @@ ALTER TABLE `user`
 -- Constraints for table `gambar`
 --
 ALTER TABLE `gambar`
-<<<<<<< HEAD
-  ADD CONSTRAINT `gambar_ibfk_1` FOREIGN KEY (`id_laundry`) REFERENCES `laundry` (`id_laundry`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `gambar_ibfk_1` FOREIGN KEY (`id`) REFERENCES `laundry` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `history`
 --
 ALTER TABLE `history`
-  ADD CONSTRAINT `history_ibfk_1` FOREIGN KEY (`id_laundry`) REFERENCES `laundry` (`id_laundry`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `history_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
-
---
--- Constraints for table `laundry`
---
-ALTER TABLE `laundry`
-  ADD CONSTRAINT `laundry_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
-=======
-  ADD CONSTRAINT `gambar_ibfk_1` FOREIGN KEY (`id`) REFERENCES `laundry` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
->>>>>>> da0ec80fa9b7f7c361960555cffee24c27043cb4
+  ADD CONSTRAINT `fk_history` FOREIGN KEY (`id`) REFERENCES `laundry` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_history1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_history2` FOREIGN KEY (`id_gambar`) REFERENCES `gambar` (`id_gambar`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
